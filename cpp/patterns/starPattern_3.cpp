04dd9a1c47140d2e9a8013d2d83175645453dabe
Ayush Anand <85148764+ayushanand09@users.noreply.github.com>
2021-10-08 13:09:25 +0530

    Add files via upload

---
+++ cpp/patterns/starPattern_3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,i,j,k;
+    cout<<"Enter the number of sides: ";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=(n-1);j>=i;j--){
+            cout<<" ";
+        }
+        for(k=1;k<=(2*i-1);k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(i=n-1;i>=1;i--){
+        for(j=i;j<=(n-1);j++){
+            cout<<" ";
+        }
+        for(k=(2*i-1);k>=1;k--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
